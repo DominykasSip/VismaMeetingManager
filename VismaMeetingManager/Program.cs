9c0993f49583c878031de3cb3b651a4306de9f0e
DominykasSip <90202955+DominykasSip@users.noreply.github.com>
2022-06-19 16:30:51 +0300

    Fixed Json path bug

--- VismaMeetingManager/Program.cs
+++ VismaMeetingManager/Program.cs
@@ -4,7 +4,8 @@ namespace VismaMeetingManager
 {
     class Program
     {
-        public static MeetingManager manager = new MeetingManager(new MeetingService(new MeetingRepository("Data.json")));
+        public static string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString() + "/Data.json";
+        public static MeetingManager manager = new MeetingManager(new MeetingService(new MeetingRepository(path)));
         public static void Main(string[] args)
         {
             manager.Start();
